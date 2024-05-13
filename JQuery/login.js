5c80280a380b1b31cb449e39ce355c013e463afd
Sebastian <sebaajara@hotmail.com>
2024-05-13 17:17:27 -0400

    Se termina formulario con error al validar el correo

--- JQuery/login.js
+++ JQuery/login.js
@@ -10,17 +10,33 @@ $(document).ready(function() {
     else{
         document.getElementById("nombre").style.border = "1px solid red"  
     }
-    if(String(pass1).length > 8){
+
+    if(String(pass1).length >= 8){
         document.getElementById("pass1").style.border = "1px solid green"  
-        if(String(pass2) == String(pass1)){
-            document.getElementById("pass2").style.border = "1px solid green"  
-        }
-        else{
-            document.getElementById("pass2").style.border = "1px solid red"  
-        }
     } else{
         document.getElementById("pass1").style.border = "1px solid red"  
     }
+    if(String(pass2) == String(pass1)){
+        document.getElementById("pass2").style.border = "1px solid green"  
+    }
+    else{
+        document.getElementById("pass2").style.border = "1px solid red"  
+    }
+     if(String(nombre) && String(email) &&String(pass1) && String(pass2)){
+
+    }
+$(document).ready(function Correo(email){
+        let expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        let valido = expReg.test(email);
+        
+        if(valido!=true){
+            alert("El correo electronico no es valido")
+        }
+        else{
+            alert("El correo electronico es valido")
+        }
+        
+    })
     $("#reg").click(function(){
         location.href = "http://127.0.0.1:3000/index.html";
         
